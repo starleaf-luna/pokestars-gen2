58fc66391538eb5a7da66c90e7f8c987a3ee5256
sabra55 <l@DESKTOP-MO78T1B.localdomain>
2023-12-24 20:50:07 +0100

    test

---
+++ engine/menus/changelog_menu.asm
@@ -0,0 +1,50 @@
+
+; CHANGELOG ROOM: accessible by holding START
+; and pressing A on the main menu screen
+
+ChangelogRoom::
+	call ClearTilemap
+	call LoadFontsExtra
+	call LoadStandardFont
+	call SetPalettes
+	call ClearWindowData
+	hlcoord 3, 0
+	ld de, ChangelogRoom_GameString
+	call PlaceString
+	hlcoord 1, 1
+	ld de, ChangelogRoom_VersionString
+	call PlaceString
+	hlcoord 1, 2
+	ld de, ChangelogRoom_IsFinalisedString
+	call PlaceString
+	hlcoord 2, 3
+	ld de, ChangelogRoom_ChangelogStartString
+	call PlaceString
+	hlcoord 1, 5
+	ld de, ChangelogRoom_Changelog
+	call PlaceString
+.loop:
+	ld a, [hJoyPressed]
+	cp B_BUTTON
+	jr z, .ok
+	cp A_BUTTON
+	jr z, .ok
+	cp START
+	jr z, .ok
+	halt
+	jr .loop
+.ok:
+	callfar MainMenu
+	
+ChangelogRoom_GameString:
+	db "{STARS_PRODNAME}@" ; POKéMON STARS
+ChangelogRoom_VersionString:
+	db "ver {STARS_VERSION} {STARS_STATUS}@" ; ver 1.0 PRE-ALPHA
+ChangelogRoom_IsFinalisedString:
+	db "is finalised?: {STARS_ISCMPLETE}@" ; is finalised?: NO
+ChangelogRoom_ChangelogStartString:
+	db "-CHANGELOG START-@"
+ChangelogRoom_Changelog:
+	db "- added Changelog"
+	next "- changed title"
+	next "- removed intro@"
